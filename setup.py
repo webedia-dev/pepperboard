f79ece798050618b5870ec40f3fbd5d1b484c760
Cyril LAVIER <cyril.lavier@davromaniak.eu>
2016-11-30 18:24:18 +0100

    Releasing...

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ with open('requirements.txt') as requirements_file:
 
 setup(
     name='pepperboard',
-    version='2.1',
+    version='2.2',
     packages=['pepperboard', 'pepperboard.core', 'pepperboard.dashboards'],
     scripts=['scripts/pepperboard'],
     url='https://github.com/webedia-dev/pepperboard',
-    download_url='https://github.com/webedia-dev/pepperboard/releases/tag/v2.1',
+    download_url='https://github.com/webedia-dev/pepperboard/releases/tag/v2.2',
     license='Apache',
     author='Cyril LAVIER',
     author_email='cyril.lavier@webedia-group.com',
